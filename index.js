d560f2e4df8c6267ecd9ebcf941739601e68648c
elzp <piechelzbieta@wp.pl>
2022-05-02 21:50:29 +0200

    Added ability to change status (showed on circles too)

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ import './style.css';
 const buttons = document.querySelectorAll('button');
 const prevButton = buttons[0];
 const nextButton = buttons[1];
+const circlesAsArray = Object.entries(
+  document.querySelectorAll('[class="flex-1"]')
+);
+
+const modifiedCircles = circlesAsArray.filter(
+  (item) =>
+    !['0', `${circlesAsArray.length - 1}`].some((item2) => item2 === item[0])
+);
+const circles = Object.fromEntries(modifiedCircles);
+console.log(typeof circles, 'circles[0]', circles[0]);
 
 const status = document.querySelectorAll(`body>div>div:nth-child(2)>div`);
 let currentStatus = 0;
@@ -15,6 +25,9 @@ prevButton.addEventListener('click', (e) => {
     prevButton.disabled = false;
 
     status[currentStatus].style.color = 'black';
+
+    circles[currentStatus + 1].innerHTML = setCircleAsUndone(currentStatus);
+
     currentStatus -= 1;
     console.log(currentStatus);
 
@@ -25,13 +38,22 @@ prevButton.addEventListener('click', (e) => {
     prevButton.disabled = true;
   }
 });
+console.log(circles[currentStatus + 1]);
 
 nextButton.addEventListener('click', (e) => {
   e.preventDefault();
   if (currentStatus < status.length - 1) {
     nextButton.disabled = false;
     status[currentStatus].style.color = 'green';
+    console.log(circles[currentStatus + 1]);
+
+    circles[currentStatus + 1].innerHTML = setCircleAsDoneOrInProgress();
+
+    // setAsUndone(currentStatus);
     currentStatus += 1;
+    circles[currentStatus + 1].innerHTML = setCircleAsDoneOrInProgress();
+    // circles[currentStatus].innerHTML = setAsUndone(currentStatus);
+
     status[currentStatus].style.color = 'orange';
     console.log(currentStatus);
   } else {
@@ -40,3 +62,18 @@ nextButton.addEventListener('click', (e) => {
     nextButton.disabled = true;
   }
 });
+/// body>div:first-child>div:first-child
+
+function setCircleAsUndone(numberOfStatus) {
+  return `<div class="w-10 h-10 bg-white border-2 border-grey-light mx-auto rounded-full text-lg text-white flex items-center">
+				<span class="text-grey-darker text-center w-full">${numberOfStatus}</span>
+			</div>`;
+}
+
+function setCircleAsDoneOrInProgress() {
+  return `<div class="w-10 h-10 bg-green mx-auto rounded-full text-lg text-white flex items-center">
+  <span class="text-white text-center w-full"><i class="fa fa-check w-full fill-current white"></i></span>
+</div>`;
+}
+
+console.log(circles);
