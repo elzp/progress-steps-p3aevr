ff53f3a6b5ab06fed09e6275f801eb23a845215e
elzp <piechelzbieta@wp.pl>
2022-05-02 20:59:20 +0200

    Fixed problem at ends of status line

--- index.js
+++ index.js
@@ -13,10 +13,15 @@ prevButton.addEventListener('click', (e) => {
   e.preventDefault();
   if (currentStatus > 0) {
     prevButton.disabled = false;
+
     status[currentStatus].style.color = 'black';
     currentStatus -= 1;
+    console.log(currentStatus);
+
     status[currentStatus].style.color = 'orange';
   } else {
+    console.log(currentStatus);
+    nextButton.disabled = false;
     prevButton.disabled = true;
   }
 });
@@ -28,7 +33,10 @@ nextButton.addEventListener('click', (e) => {
     status[currentStatus].style.color = 'green';
     currentStatus += 1;
     status[currentStatus].style.color = 'orange';
+    console.log(currentStatus);
   } else {
+    console.log(currentStatus);
+    prevButton.disabled = false;
     nextButton.disabled = true;
   }
 });
