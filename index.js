e5e4b6c9db890c432098c73f3064967e2c04c357
elzp <piechelzbieta@wp.pl>
2022-05-02 20:46:14 +0200

    Added ability to change status (showed on text)

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
 // Import stylesheets
 import './style.css';
+
+const buttons = document.querySelectorAll('button');
+const prevButton = buttons[0];
+const nextButton = buttons[1];
+
+const status = document.querySelectorAll(`body>div>div:nth-child(2)>div`);
+let currentStatus = 0;
+status[0].style.color = 'orange';
+
+prevButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (currentStatus > 0) {
+    prevButton.disabled = false;
+    status[currentStatus].style.color = 'black';
+    currentStatus -= 1;
+    status[currentStatus].style.color = 'orange';
+  } else {
+    prevButton.disabled = true;
+  }
+});
+
+nextButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (currentStatus < status.length - 1) {
+    nextButton.disabled = false;
+    status[currentStatus].style.color = 'green';
+    currentStatus += 1;
+    status[currentStatus].style.color = 'orange';
+  } else {
+    nextButton.disabled = true;
+  }
+});
