6e61f14458e0784bf0d42574cc7edacd7a095862
elzp <piechelzbieta@wp.pl>
2022-05-05 18:57:08 +0200

    Compleated the task

--- index.js
+++ index.js
@@ -13,56 +13,66 @@ const modifiedCircles = circlesAsArray.filter(
     !['0', `${circlesAsArray.length - 1}`].some((item2) => item2 === item[0])
 );
 const circles = Object.fromEntries(modifiedCircles);
-console.log(typeof circles, 'circles[0]', circles[0]);
 
 const status = document.querySelectorAll(`body>div>div:nth-child(2)>div`);
+const progressLines = document.querySelectorAll('div.w-full');
+
 let currentStatus = 0;
 status[0].style.color = 'orange';
 
+circles[1].innerHTML = setCircleAsUndone(1);
+circles[2].innerHTML = setCircleAsUndone(2);
+progressLines[0].innerHTML = setLineAsGreen(0);
+progressLines[1].innerHTML = setLineAsGreen(0);
+
 prevButton.addEventListener('click', (e) => {
   e.preventDefault();
-  if (currentStatus > 0) {
+  if (currentStatus > -1) {
     prevButton.disabled = false;
-
-    status[currentStatus].style.color = 'black';
-
-    circles[currentStatus + 1].innerHTML = setCircleAsUndone(currentStatus);
-
-    currentStatus -= 1;
-    console.log(currentStatus);
-
+    circles[currentStatus + 1].innerHTML = setCircleAsUndone(currentStatus + 1);
     status[currentStatus].style.color = 'orange';
+
+    if (currentStatus > 0) {
+      progressLines[currentStatus - 1].innerHTML = setLineAsGreen(20);
+    }
+
+    if (currentStatus < status.length - 1) {
+      status[currentStatus + 1].style.color = 'black';
+      progressLines[currentStatus].innerHTML = setLineAsGreen(0);
+    }
+
+    if (currentStatus !== 0) {
+      currentStatus -= 1;
+    }
   } else {
-    console.log(currentStatus);
     nextButton.disabled = false;
     prevButton.disabled = true;
   }
 });
-console.log(circles[currentStatus + 1]);
 
 nextButton.addEventListener('click', (e) => {
   e.preventDefault();
-  if (currentStatus < status.length - 1) {
-    nextButton.disabled = false;
-    status[currentStatus].style.color = 'green';
-    console.log(circles[currentStatus + 1]);
+  prevButton.disabled = false;
+  if (currentStatus < status.length) {
+    circles[currentStatus + 1].innerHTML = setCircleAsDone();
 
-    circles[currentStatus + 1].innerHTML = setCircleAsDoneOrInProgress();
-
-    // setAsUndone(currentStatus);
-    currentStatus += 1;
-    circles[currentStatus + 1].innerHTML = setCircleAsDoneOrInProgress();
-    // circles[currentStatus].innerHTML = setAsUndone(currentStatus);
-
-    status[currentStatus].style.color = 'orange';
-    console.log(currentStatus);
+    if (currentStatus !== status.length) {
+      nextButton.disabled = false;
+      status[currentStatus].style.color = 'green';
+      if (currentStatus > 0) {
+        progressLines[currentStatus - 1].innerHTML = setLineAsGreen(100);
+      }
+    }
+    if (currentStatus !== status.length - 1) {
+      status[currentStatus + 1].style.color = 'orange';
+      progressLines[currentStatus].innerHTML = setLineAsGreen(20);
+      currentStatus += 1;
+    }
   } else {
-    console.log(currentStatus);
     prevButton.disabled = false;
     nextButton.disabled = true;
   }
 });
-/// body>div:first-child>div:first-child
 
 function setCircleAsUndone(numberOfStatus) {
   return `<div class="w-10 h-10 bg-white border-2 border-grey-light mx-auto rounded-full text-lg text-white flex items-center">
@@ -70,10 +80,12 @@ function setCircleAsUndone(numberOfStatus) {
 			</div>`;
 }
 
-function setCircleAsDoneOrInProgress() {
+function setCircleAsDone() {
   return `<div class="w-10 h-10 bg-green mx-auto rounded-full text-lg text-white flex items-center">
   <span class="text-white text-center w-full"><i class="fa fa-check w-full fill-current white"></i></span>
 </div>`;
 }
 
-console.log(circles);
+function setLineAsGreen(valueOfPercentages) {
+  return `<div class="bg-green-light text-xs leading-none py-1 text-center text-grey-darkest rounded " style="width: ${valueOfPercentages}%"></div>`;
+}
